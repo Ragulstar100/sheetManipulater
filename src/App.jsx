22aa3e22d0f660dce22b56edbc6000ca73e70aab
ragul <ragulstar100@gmail.com>
2025-07-25 13:33:49 +0530

    bar chart finshed

--- src/App.jsx
+++ src/App.jsx
@@ -3,57 +3,34 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import * as echarts from 'echarts';
 import './App.css'
-import {
-  Chart as ChartJS,
-  LineElement,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  Title,
-  Tooltip,
-  Legend,
-  RadialLinearScale,
-  ArcElement,
-} from 'chart.js';
 import { Line, PolarArea } from 'react-chartjs-2';
 import { ExcelCsvReader, excelDateToJSDate } from './assets/ExcelCsvReader';
 
 
-ChartJS.register(RadialLinearScale, ArcElement, Tooltip, Legend);
 
-const GameData={
-  score:"Score",
-  dataPlayed:"Date Played",
-  gameName:'Game Name',
-  levelReached:'Level Reached',
-  timePlayed:'Time Played'
-}
 
+const GameData={ score:"Score", dataPlayed:"Date Played", gameName:'Game Name',levelReached:'Level Reached',timePlayed:'Time Played',playerName:'Player Name'}
+
+//Filter My Raw data
 function gameFilterData(data){
-{
+
   let _labels=Array.from(new Set(data.map((e)=>e[GameData.gameName])))
   let _maxArray=[]
   _labels.forEach((n)=>{
     _maxArray.push(Math.max(...data.filter((e=>e[GameData.gameName]==n)).map((e)=>e[GameData.levelReached])))
   })
+
   return{
     labels:_labels,
+    playerName:Array.from(new Set(data.map((e)=>e[GameData.playerName]))),
     maxArray:_maxArray,
-    filterEven:()=> fiterEvenDays((data||[]).map((e)=> e[GameData.dataPlayed]),3).map((e,i)=>i*3)
-  }
-}
-}
+    filterEven:()=> fiterEvenDays((data||[]).map((e)=> e[GameData.dataPlayed]),3).map((e,i)=>i*3)}}
 
+
+//Filter My Data Flow Chart
 function dataFilter(data){
-
-
 let obj=()=>gameFilterData(data)
-
-
-
-const primaryData= ['product',...obj().filterEven()]
-
-    const option = {legend: {},
+const timeData = {legend: {},
  tooltip: {
         trigger: 'axis',
         showContent: false
@@ -62,12 +39,8 @@ const primaryData= ['product',...obj().filterEven()]
         source: [
           ['Games'].concat(obj().filterEven()),
           ...obj().labels.map((e,i)=>{
-            return  [obj().labels[i]].concat(data.filter((e)=>e[GameData.gameName]==obj().labels[i]).map((e)=>(  parseInt(e[GameData.timePlayed].split(' ')[0])) ))
-          }),
-          [obj().labels[0]].concat(data.filter((e)=>e[GameData.gameName]==obj().labels[0]).map((e)=>(  parseInt(e[GameData.timePlayed].split(' ')[0])) )),
-          ['Matcha Latte', 51.1, 51.4, 55.1, 53.3, 73.8, 68.7],
-          ['Cheese Cocoa', 40.1, 62.2, 69.5, 36.4, 45.2, 32.5],
-          ['Walnut Brownie', 25.2, 37.1, 41.2, 18, 33.9, 49.1]
+            return  [obj().labels[i]].concat( data.filter((e)=>e[GameData.gameName]==obj().labels[i]).map((e)=>(  parseInt(e[GameData.timePlayed].split(' ')[0])) ))
+          })
         ]
       },
       xAxis: { type: 'category' },
@@ -119,10 +92,39 @@ const primaryData= ['product',...obj().filterEven()]
     };
 
 
+
+   let barChartBulider= (()=>{
+          let list=[]
+          obj().playerName.forEach((e)=>{
+            let local={"Games":e}
+              data.forEach((g)=>{
+              local[g[GameData.gameName]] = data.filter((f)=>f[GameData.gameName]==g[GameData.gameName]).reduce((acc,r)=>acc+parseInt(r[GameData.timePlayed].split(' ')[0]),0)
+              })
+            list.push(local)  
+          })
+          console.log(obj().labels)
+          return list
+    })
+
+
 return {
   rawData:data,
   update:()=>{dataFilter(data);return data},
-  option:option,
+  timePalyedData:timeData,
+  totalPlayedData:{
+  legend: {},
+  tooltip: {},
+  dataset: {
+    dimensions: ['Games',...obj().labels],
+    source:barChartBulider()
+  
+  },
+  xAxis: { type: 'category' },
+  yAxis: {},
+  // Declare several bar series, each will be mapped
+  // to a column of dataset.source by default.
+  series: [{ type: 'bar' }, { type: 'bar' }, { type: 'bar' },{type:'bar'}]
+},
   lineCharData:{
       title: {
         text: 'Line Chart Example'
@@ -139,7 +141,7 @@ return {
       },
       series: [
         {
-          name: 'Sales',
+          name: 'Game score',
           type: 'line',
           data: data.map((e)=>e[GameData.score]),
           smooth: true
@@ -150,7 +152,7 @@ return {
   poloarAreaChartData:{
   angleAxis: {
     type: 'category',
-    data: obj().labels
+    data: obj().labels.concat('blue')
   },
   radiusAxis: {},
   polar: {},
@@ -175,6 +177,8 @@ return {
 
 }
 
+
+
  function fiterEvenDays(data,n) {
   
   const dates = data.sort((a, b) => a - b);           
@@ -194,11 +198,15 @@ return {
 
 
 
+
+
 function App() {
   const [data, setdata] = useState([])
 
+  const [name,setName]=useState('All')
+
   useEffect(()=>{
-    console.log(data.map((e)=>(  parseInt(e[GameData.timePlayed].split(' ')[0])) ))
+   // console.log(data.map((e)=>(  parseInt(e[GameData.timePlayed].split(' ')[0])) ))
   },[data]
 )
 
@@ -206,16 +214,34 @@ function App() {
 
   return (
     <>
-    <div className='flex items-center'>
-    <Chart className='w-200 h-75' data={dataFilter(data).lineCharData}/>
+    <div className='flex flex-col'>  
 
-    <Chart className='w-100 h-50' data={dataFilter(data).poloarAreaChartData}/>
+    <Dropdown className='self-end' data={gameFilterData(data).playerName} onSelect={(v)=>{
+      setName(v)
+    }}/>  
+
+    <div className='flex items-center'>  
+
+    <Chart className='w-200 h-75' data={dataFilter(data.filter((e)=>{
+      return name=='All'||e[GameData.playerName]==name
+    })).lineCharData}/>
+
+    <Chart className='w-100 h-50' data={dataFilter(data.filter((e)=>{
+      return name=='All'||e[GameData.playerName]==name
+    })).poloarAreaChartData}/>
+
     </div>
-    <ChartCombo data={dataFilter(data).option}/>
+    </div>
+    <ChartCombo data={dataFilter(data).timePalyedData}/>
+
+   <Chart className='w-screen h-150' data={dataFilter(data.filter((e)=>{
+      return name=='All'||e[GameData.playerName]==name
+    })).totalPlayedData}/>
 
     <ExcelCsvReader key={2} getData={(d)=>{
       setdata(d)
     }}/>
+
 
     <Dropdown/>
 
@@ -224,6 +250,8 @@ function App() {
     </>
   )
 }
+
+
 
 
 
@@ -285,17 +313,21 @@ function ChartCombo({data}) {
 };
 
 
-export function Dropdown({ className, data }) {
+export function Dropdown({ className, data,label,onSelect }) {
   const [selected, setSelected] = useState('');
 
   return (
     <div className={className || 'w-64'}>
       <label className="block mb-2 text-sm font-medium text-gray-700">
-        Choose an option
+        {label||'Select Your Option'}
       </label>
       <select
         value={selected}
-        onChange={(e) => setSelected(e.target.value)}
+        onChange={(e) =>{
+           setSelected(e.target.value)
+           onSelect(e.target.value)
+        }
+        }
         className="block w-full px-4 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500 bg-white text-gray-700"
       >
         <option value="All">
